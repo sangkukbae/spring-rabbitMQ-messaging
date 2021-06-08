7856eab1895a948e5ff154a4db726b33e81b0921
benwizschool <ben@wizschool.io>
2021-06-09 00:28:27 +0900

    Fix formating

--- src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -8,41 +8,44 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootApplication
 public class MessagingRabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
+    static final String topicExchangeName = "spring-boot-exchange";
 
-	static final String queueName = "spring-boot";
+    static final String queueName = "spring-boot";
 
-	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
-	}
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
 
-	@Bean
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
     Binding binding(Queue queue, TopicExchange exchange) {
-    return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-  }
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
 
-  @Bean
-  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                           MessageListenerAdapter listenerAdapter) {
-    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-    container.setConnectionFactory(connectionFactory);
-    container.setQueueNames(queueName);
-    container.setMessageListener(listenerAdapter);
-    return container;
-  }
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
 
-  @Bean
-  MessageListenerAdapter listenerAdapter(Receiver receiver) {
-    return new MessageListenerAdapter(receiver, "receiveMessage");
-  }
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
 
 	public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
